62de113353de18b83f487c8bcc3f8503d83678d0
Mohamed Bakry <bakry5@users.noreply.github.com>
2026-07-26 15:05:00 +0300

    feat: support Bearer token in protect middleware alongside cookie auth

--- services/authService.js
+++ services/authService.js
@@ -52,8 +52,10 @@ exports.logout = async (req, res, next) => {
 
 exports.protect = async (req, res, next) => {
   let token;
- 
-   if (req.cookies && req.cookies.token) {
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies && req.cookies.token) {
     token = req.cookies.token;
   }
 
